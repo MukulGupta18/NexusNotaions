4ca91056a602704a56fdeb4221dec5ee6eb07f6f
Mukul Gupta <mgofficial20@gmail.com>
2024-07-10 23:54:29 +0530

    commit

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -8,6 +8,8 @@ const Noteitem = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
+                    <i class="fa-solid fa-trash mx-2"></i>
+                    <i class="far fa-edit mx-2"></i>
                 </div>
             </div>
         </div>
